4fc9f2284be7d5f7d4c7eca3f3ad95c86dab1b6b
JustaNewbee <844266359@qq.com>
2018-08-02 00:06:45 +0800

    init

---
+++ application/index/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+use think\Session;
+class Index extends Controller{
+    public function index() {
+        Session::start();
+        $uname = Session::get('uname');
+        if(!$uname) {
+            $this->redirect('./Login/index');
+            return;
+        }
+        $this->assign('user', $uname);
+        return $this->fetch();
+    }
+}
